0c2116a9531b9a6ecd0e3e5f513b36ea1e72d76f
BakiSavas27 <savasbaki25@gmail.com>
2023-12-22 10:24:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import Cards from './Cards'
+
+function App() {
+
+  return (
+    <div className='wrapper'>
+      
+<Cards
+img="https://img-s1.onedio.com/id-5f6f4d85e123d2a3799f7750/rev-0/w-1200/h-700/f-jpg/s-2fe8f005ed4456bcb2337c4fbc98593b64ffb978.jpg"
+title="Aristotales"
+description="Aristotle, B.C. He is a philosopher of Greek origin, born in 384. He is also the most influential person who ever lived.."
+/>
+
+<Cards
+img="https://img-s1.onedio.com/id-5f6f5d0a6f7551ae223c7860/rev-0/w-1200/h-632/f-jpg/s-dbd7778b274458e334f633729fac83c57053a8f0.jpg"
+title="Leonardo da Vinci"
+description="Leonardo da Vinci was undoubtedly the most talented man of his time.
+"
+/>
+<Cards
+img="https://img-s1.onedio.com/id-5f6f5eca3b09945225592ede/rev-0/w-1200/h-882/f-jpg/s-8bc8a899ef036af31c999e20df5e64372aa333bb.jpg"
+title="Galileo Galilei"
+description="Called the father of modern physics, Galileo Galilei was more than a great scientist and astronomer; he was literally a rebel
+"
+/>
+
+
+
+      
+    </div>
+  )
+}
+
+export default App
